2a3154e8f945d79b8a34738cd3153a4b582acbe3
K1MP4 <alextiloup@gmail.com>
2024-10-22 12:29:34 +0200

    SpringBootInitialized

---
+++ src/main/java/com/vedruna/alumnos1_1/Alumnos11Application.java
@@ -0,0 +1,13 @@
+package com.vedruna.alumnos1_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Alumnos11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Alumnos11Application.class, args);
+	}
+
+}
